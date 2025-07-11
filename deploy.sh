3e95aa8b8c56bb7fecf60f896c086d4a6096151b
lukman.fyrtio <lukman.fyrtio@gmail.com>
2025-07-11 23:43:56 +0700

    https

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Nama file CAR
+CAR_FILE="./target/poc-wso2mi-telkom_1.0.0.car"
+
+# Tujuan remote
+REMOTE_USER="swamedia"
+REMOTE_HOST="localswa"
+REMOTE_PATH="/home/swamedia/apps/wso2mi-4.4.0-wso2dev/repository/deployment/server/carbonapps"
+
+# Jalankan SCP
+echo "Uploading $CAR_FILE to $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
+scp "$CAR_FILE" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
+
+# Cek hasil
+if [ $? -eq 0 ]; then
+  echo "✅ Deploy sukses!"
+else
