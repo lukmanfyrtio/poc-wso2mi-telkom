b9897b12df4a40a3ed25ca3a75f6cf6ea3cfd3fb
Krisna Rahmat <krisnarahmat1@gmail.com>
2025-12-31 14:59:09 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ CAR_FILE="./target/poc-wso2mi-telkom_1.0.0.car"
 # Tujuan remote
 REMOTE_USER="swamedia"
 REMOTE_HOST="localswa"
-REMOTE_PATH="/home/swamedia/apps/wso2mi-4.4.0-wso2dev/repository/deployment/server/carbonapps"
+REMOTE_PATH="/home/swamedia/apps/wso2mi-4.4.0-telkom/repository/deployment/server/carbonapps"
 
 # Jalankan SCP
 echo "Uploading $CAR_FILE to $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
